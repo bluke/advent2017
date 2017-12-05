b712bde2028fd16d54f7272212f9ad6c9457de66
Luke Bertot <lbertot@unistra.fr>
2017-12-05 16:04:59 +0100

    Day 4

---
+++ day4_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+def get(filename):
+    res = []
+    with open(filename) as fd :
+        for line in fd :
+            res.append(line.rstrip().split())
+    return res
+
+def validate(array):
+    res = 0
+    print(len(array))
+    for line in array:
+        line.sort()
+        ok=True
+        i=1
+        while i<len(line) and ok:
+            if line[i]==line[i-1]:
+                res+=1
+                ok=False
+            i+=1
+    print(res)
+    return len(array)-res
+
+
+
+
+data = get("day4.in")
+print(validate(data))
